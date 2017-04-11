bccff2d397c31db15c2b0a265cef2a70c4922a09
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-11 18:37:57 -0400

    adds react-router for blogged and renders app component on onload

--- blogged/src/index.js
+++ blogged/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {Router, browserHistory} from 'react-router';
 
-import App from './components/app';
 import reducers from './reducers';
+
+import {RootRoute} from './routes/Root.route.js';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  	<Router history={browserHistory} routes={RootRoute} />
   </Provider>
   , document.querySelector('.container'));
---
+++ blogged/src/routes/Root.route.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+import App from './../components/App.component.jsx';
+
+const routes = (
+	<Route path="/" component={App}>
+		
+	</Route>
+)
+
+export const RootRoute = routes;
